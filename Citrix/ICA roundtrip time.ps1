ce995624f5fb869a99d804aaecb62a6a40a47344
its\epierik <erik.pierik@centric.eu>
2019-10-14 21:03:53 +0200

    Citrix

---
+++ Citrix/ICA roundtrip time.ps1
@@ -0,0 +1,126 @@
+﻿#Script to calculate the ICA Roundtrip time 
+#Created by Erik Pierik on 29th of September 2016
+<#
+.Synopsis
+   This CMDlet gets the ICA roundtrip time from a user. This is helpfull to determine if there is a problem on the network when a user reports
+   slowness in his session. The cmdlet has to be run from a delivery controller. This script is intended to be used on XenApp 7.x although it might also
+   work on other XenDesktop this is not tested. 
+.DESCRIPTION
+   Sometimes users report slowness in Citrix but on the server itself nothing can explain this slowness (CPU, Memory and Disk usage are good) then the issue
+   might be something on the network. The ICA roundtrip time shows the latency between the user action (typing a letter or moving the mouse) and that action 
+   showing on the screen of the user. 
+.EXAMPLE
+   Get-ICARoundTripTimeUser -username samaccountname for example Get-ICARoundTripTimeUser -username bj631q
+#>
+Function Get-ICARoundTripTimeUser
+{
+[CmdletBinding()]
+
+Param (
+       [parameter(Mandatory=$true)]
+       $userName
+       
+      )
+
+Begin
+{
+    #For the query user command we only need the samaccountname. For the query of the brokersession we need to add the domain before the user
+    #instead of putting a domain in from we use a wildcard so we have to modify the variable to add a * before the username. This way the
+    #brokersession can be queried. 
+    $userName2 = "*" + $username
+    $machineName = "*" + $machineName 
+    $loadedSnappins = get-pssnapin 
+    #Check if the Citrix snappins are loaded, if not load them
+        if (-not $loadedSnappins.tostring().contains("citrix"))
+            {
+                Add-PSSnapin *citrix*
+            }
+
+     
+            $sessions = get-BrokerSession -BrokeringUserName $userName2
+}
+Process 
+{
+    #sometimes a user might have multiple sessions, to make sure we get the right information for all the sessions we put the sessions in a foreach loop
+    foreach ($session in $sessions)
+        {
+            $substring = $session.MachineName.tostring().IndexOf("\")
+            $Server = $session.machinename.tostring().Substring($substring+1)
+
+            $users = query user $userName /server:$server
+            $SessionID = $users[1].ToString().substring(42,2)
+        
+
+            $roundTrip = Get-CimInstance -ComputerName $server -Namespace root\citrix\euem -ClassName citrix_euem_RoundTrip|where {$_.sessionID -eq $sessionID}
+        
+            "The ICA rounttrip time for the user " + $userName + " is " + $roundTrip.roundtriptime + " milliseconds on server " + $Server
+        
+        }
+}
+
+#end function
+}
+
+<#
+.Synopsis
+   This CMDlet gets the ICA roundtrip time for all users on a server. This is helpfull to determine if there is a problem on the network when a user reports
+   slowness in his session. The cmdlet has to be run from a delivery controller. This script is intended to be used on XenApp 7.x although it might also
+   work on other XenDesktop this is not tested. 
+.DESCRIPTION
+   Sometimes users report slowness in Citrix but on the server itself nothing can explain this slowness (CPU, Memory and Disk usage are good) then the issue
+   might be something on the network. The ICA roundtrip time shows the latency between the user action (typing a letter or moving the mouse) and that action 
+   showing on the screen of the user. 
+.EXAMPLE
+   Get-ICARoundTripTimeServer -MachineName hostnameOfXenAppServer for example Get-ICARoundTripTimeServer -machineName usflmia-ts10
+#>
+
+Function Get-ICARoundTripTimeServer
+{
+[CmdletBinding()]
+
+Param (
+       [parameter(Mandatory=$true)]
+       $machineName = "localhost"
+       
+      )
+
+Begin
+{
+    #Check if the snapins are loaded, if not load them first before continuing 
+    $loadedSnappins = get-pssnapin 
+    #Check if the Citrix snappins are loaded, if not load them
+        if (-not $loadedSnappins.tostring().contains("citrix"))
+            {
+                Add-PSSnapin *citrix*
+            }
+
+    $users = query user /server:$machineName 
+    $machineName = "*" + $machineName 
+                
+}
+Process 
+{
+    #Loop through all the users that came up by the query user command. The if statement is to only capture users that are connected through ica
+    #we need to use the query User command first because the wmi object only handles the session ID. The get-brokersession command does not provide this
+    foreach ($user in $users)
+        {
+            if ($user.substring(23,1) -eq "i")
+                {
+                    $SessionID = $user.ToString().substring(42,2)
+                    $index = $user.substring(1).indexof(" ")
+                    $userName = "*" + $user.ToString().substring(1,$index)
+            
+                    $session = get-BrokerSession -BrokeringUserName $userName|where {$_.MachineName -like $machineName }
+                    $subString = $session.BrokeringUserName.tostring().indexof("\")
+                    $userName = $session.BrokeringUserName.ToString().Substring($substring+1)
+            
+                    $roundTrip = Get-CimInstance -ComputerName $machineName.Substring(1) -Namespace root\citrix\euem -ClassName citrix_euem_RoundTrip|where {$_.sessionID -eq $sessionID}
+        
+                    "The ICA rounttrip time for the user " + $userName + " is " + $roundTrip.roundtriptime + " milliseconds on server " + $Server
+               }
+        
+        }
+}
+
+#end function
+}
---
+++ Citrix/RDS and Citrix plus Application install.ps1
@@ -0,0 +1,85 @@
+﻿#Script to install application on Citrix in the right order
+
+#Install the Remote Desktop Role and then reboot. After the reboot continue with the rest of the installation. 
+import-module servermanager
+
+$RDSInstalled = Get-WindowsFeature | where {$_.installed -eq $true -and $_.name -like "RDS-RD-Server"}
+If ($RDSInstalled -eq $null)
+    {
+        write-host "Now installing the Remote Desktop server role, after that a restart will happen after which the installation will continue" -ForegroundColor Yellow
+        write-host "You will need to logon to the server using the same credentials to let the installation continue automatically" -ForegroundColor Yellow
+        add-windowsfeature rds-rd-server
+        
+        #after installation of the RDS server role the server needs to be restarted 
+        'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "d:\genPact_main.ps1"'|out-file "d:\installation.cmd" -Encoding ascii
+        $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce\"
+        $script = "d:\installation.cmd"
+        new-item -path $registryPath -erroraction silentlycontinue
+        new-itemproperty -path $registryPath -name script -value $script -PropertyType string -force
+                sleep 10
+        Restart-Computer -force
+    }
+
+#Put the server into install mode 
+write-host "Placing this terminal server into install mode" -ForegroundColor Yellow
+change user /install
+
+#Install Internet Explorer and plugins
+write-host "Now installing Internet Explorer 11" -ForegroundColor Yellow
+Start-Process "D:\Installables\IE11-Windows6.1-x64-en-us.exe" -argumentlist "/passive /norestart" -wait
+write-host "Now installing the Internet Explorer Okta Plugin" -ForegroundColor Yellow
+start-process "C:\Windows\System32\wscript.exe" -argumentlist "D:\Installables\Unisys_packages\Okta_IE_Plugin_5.2.2\Okta_IE_Plugin_5.2.2_Install.vbs" -wait
+
+#Install the Oracle Client
+write-host "Now installing the Oracle 11G client, this will take some time" -ForegroundColor Yellow
+Start-Process "C:\Windows\System32\cmd.exe" -argumentlist "/c D:\Installables\Unisys_packages\Oracle11g_Win7\Install.cmd" -wait
+
+#Install Office and Lync
+write-host "Now installing Office 2010 and Lync 2010" -ForegroundColor Yellow
+start-process "D:\Installables\Office_Prof_Plus_2010w_SP1_x86_English\setup.exe" -argumentlist "/adminfile D:\Installables\Office_Prof_Plus_2010w_SP1_x86_English\citrix_full.msp" -wait
+start-process "D:\Installables\Lync_2010_64Bit\LyncSetup.exe" -ArgumentList "/install /silent" -Wait
+
+#start-process "C:\Windows\System32\wscript.exe" -argumentlist "D:\Installables\Unisys_packages\Microsoft_Office_Professional_Plus_2013_ENG\Microsoft_Office_Professional_Plus_2013_ENG_Install.vbs" -wait
+#Start-Process "C:\Windows\System32\wscript.exe" -argumentlist "d:\installables\unisys_packages\microsoft_lync_client_2013\Microsoft_lync_client_2013_install.vbs" -wait
+
+#Install Java
+write-host "Now installing Java 1.6" -ForegroundColor Yellow
+Start-Process "D:\Installables\Java\jre-6u45-windows-x64.exe" -argumentlist "/s" -wait
+
+write-host "Now installing Java 1.7" -ForegroundColor Yellow
+Start-Process "D:\Installables\Java\jre-7u80-windows-x64.exe" -argumentlist "/s WEB_JAVA_SECURITY_LEVEL=m" -wait
+
+#write-host "Now installing Java 1.8" -ForegroundColor Yellow
+#Start-Process "D:\Installables\Java\jre-8u73-windows-x64.exe" -argumentlist "/s INSTALLCFG=D:\Installables\Java\JRE-8-SilentInstalloptions.cfg" -wait
+
+#Install Adobe Reader
+write-host "Now installing Adobe Reader" -ForegroundColor Yellow
+start-process "C:\Windows\System32\wscript.exe" -argumentlist "D:\Installables\Unisys_packages\Adobe_Reader_DC_15.009_ENG\Adobe_Reader_DC_15.009_ENG_Install.vbs" -wait
+
+#Install SAP
+write-host "Now installing SAP" -ForegroundColor Yellow
+Start-Process "C:\Windows\System32\cmd.exe" -argumentlist "/c d:\installables\SAP_GUI_7.2_Win7_unisys\install.cmd" -wait
+
+#Install Winzip
+write-host "Now installing Winzip 12" -ForegroundColor Yellow
+Start-Process "C:\Windows\System32\wscript.exe" -argumentlist "D:\Installables\Unisys_packages\Winzip12.0\Winzip.vbs" -wait
+
+#Install Citrix VDA
+write-host "Now installing the Citrix VDA version 7.8" -ForegroundColor Yellow
+start-process "c:\windows\system32\cmd.exe" -ArgumentList "/k D:\Installables\Citrix\VDAServerSetup_7.8.exe /components vda,plugins /noreboot /passive /controllers usflmia-is11.northamerica.delphiauto.net /enable_HDX_3D_PRO /ENABLE_REMOTE_ASSISTANCE /OPTIMIZE" -wait
+
+#Put the server back into execution mode
+Change user /execute
+write-host "Installation complete, you need to restart te server to make all the settings effective" -ForegroundColor Yellow
+
+write-host "All the applications are installed. Now please reboot the server to finish the configuration Press y to reboot" -ForegroundColor Yellow
+$reboot = read-host 
+    if ($reboot  -eq "y")
+        {
+            Restart-Computer -force
+        }
+
+#Copy the PSWindowsupdate module and install the windows updates
+write-host "Installing Microsoft Updates, this will take some time" -ForegroundColor Yellow
+import-module pswindowsupdates
+get-wuinstall -IgnoreReboot -AcceptAll
